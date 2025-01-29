e61764e6b358d78485eee2c789a6d788181d6a25
SpetnixDev <spetnix@gmail.com>
2025-01-29 18:35:33 +0100

    Resolved caching issues

--- src/main/java/com/oc/projet7app/controller/ProfileController.java
+++ src/main/java/com/oc/projet7app/controller/ProfileController.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
 	@GetMapping
-	public String profile(HttpServletRequest request) {
+	public String profile(HttpServletRequest request, HttpServletResponse response) {
 		if (request.getSession().getAttribute("user") != null) {
+			response.setHeader("pragma", "no-cache");              
+			response.setHeader("Cache-control", "no-cache, no-store, must-revalidate");             
+			response.setHeader("Expires", "0");
+			
 			return "profile";
 		}
 		
